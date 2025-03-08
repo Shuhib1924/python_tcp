b01ba0d4a34f5cab3491be2a7cd2498dc15f6274
Shuhib <shuhib_s@live.de>
2025-03-08 10:52:43 +0100

    clirnt connection

---
+++ client.py
@@ -0,0 +1,5 @@
+import socket
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client_socket.connect((socket.gethostbyname(socket.gethostname()), 12345))
--- main.py
+++ main.py
@@ -2,3 +2,17 @@ import socket
 
 print(socket.gethostname())
 print(socket.gethostbyname(socket.gethostname()))
+
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((socket.gethostbyname(socket.gethostname()), 12345))
+server_socket.listen()
+print("server started")
+while True:
+    client_socket, client_address = server_socket.accept()
+    print(type(client_socket))
+    print(client_socket)
+    print(type(client_address))
+    print(client_address)
+    client_socket.close()
+    print(f"Connected to {client_address}")
